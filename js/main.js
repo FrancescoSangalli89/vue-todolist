32f239a9fe1c017a24e7d8593c20fe6940f62ed3
Francesco Sangalli <francesco.sangalli89@gmail.com>
2022-07-25 20:59:53 +0200

    finish: Vue To Do List work

---
+++ js/main.js
@@ -0,0 +1,27 @@
+const app = new Vue({
+    el: '#app',
+    data: {
+        newToDo: '',
+        // done: false,
+        toDos: []
+    },
+    methods: {
+        cancelToDo(position) {
+            this.toDos.splice(position, 1);
+        },
+        addToDo() {
+            if (this.newToDo != '') {
+                this.toDos.push(
+                    {
+                        'text': this.newToDo.trim(),
+                        'done': false
+                    }
+                );
+            }
+            this.newToDo = '';
+        },
+        doneTrue(position) {
+            this.toDos[position].done = !this.toDos[position].done;
+        }
+    }
+})
